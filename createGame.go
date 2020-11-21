57c4308e1f3134fd319a0a71b38e7c1828fd7d7c
ericwedemire <ericwedemire@gmail.com>
2020-11-21 12:53:47 -0700

    createGame adds Websocket connection to inital user

--- createGame.go
+++ createGame.go
@@ -7,19 +7,59 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-redis/redis"
 )
 
 // createGame spins up a new WebSocket for a new game to be created
 func createGame(writer http.ResponseWriter, request *http.Request) {
-	//connection, err := websocket.Upgrade(writer, request, nill)
-	/* this will upgrade an HTTP connection to a WebSocket Connection  */
-	var game Game
-	go game.subscribe()
+	// grabbing gameID
+	var err error
+	var newGame Game
+
+	err = json.NewDecoder(request.Body).Decode(&newGame)
+	if err != nil {
+		http.Error(writer, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		log.Println("Failure to decode client JSON:", err)
+		return
+	}
+	log.Println("Creating game with ID:", newGame.GameID)
+
+	//set game name for new game and add it to map of all active games
+	activeGames[newGame.GameID] = &newGame
+
+	// generate words and place them into database object -----------------------------------------------------------------
+	vals := map[string]interface{}{
+		"score":     "1-9",
+		"turn":      "red",
+		"redW":      "africa !agent !air alien amazon",
+		"blueW":     "angel antarctica apple arm back",
+		"assassinW": "band",
+		"civilianW": "tree plant iron",
+	}
+
+	database.HSet(ctx, "newGame", vals)
+
+	get := database.HGetAll(ctx, "newGame")
+	if err := get.Err(); err != nil {
+		if err == redis.Nil {
+			fmt.Println("key does not exists")
+		}
+		panic(err)
+	}
+	// generate words and place them into database object -----------------------------------------------------------------
+
+	//subscribe to database connection
+	log.Println("Successfully created game:", newGame.GameID)
+	go newGame.subscribe()
 }
 
+//
+//
 func (game *Game) subscribe() {
 	log.Println("starting subscriber for " + game.GameID)
 	subscriber := database.Subscribe(ctx, game.GameID)
--- socket.go
+++ socket.go
@@ -4,17 +4,46 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 )
 
 // newSocketConnection
 //
 func newSocketConnection(writer http.ResponseWriter, request *http.Request) {
+	// grabbing gameID
+	vars := mux.Vars(request)
+	gameID := vars["gameID"]
+
+	currentGame := activeGames[gameID]
+	var newUser User
+	var err error
+
+	//creating WebSocket
 	log.Println("recieved websocket request")
-	connection, err := websocket.Upgrade(writer, request, writer.Header(), 1024, 1024)
+	newUser.socket, err = upgrader.Upgrade(writer, request, nil)
 	if err != nil {
 		http.Error(writer, "Could not open websocket connection", http.StatusBadRequest)
 	}
-	var newUser User
-	newUser.socket = connection
+
+	currentGame.Connections = append(currentGame.Connections, newUser)
+	newUser.socket.WriteMessage(1, []byte("Connection Successful"))
+	go listenOnSocket(newUser.socket)
+}
+
+//
+//
+func listenOnSocket(connection *websocket.Conn) {
+	for {
+		messageType, p, err := connection.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		if err := connection.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+
 }
